f1338039fd48d516d21cb9f178a1df29a0c6173c
wonho1111 <109856968+wonho1111@users.noreply.github.com>
2023-12-19 01:27:45 +0900

    music class and music list implement, title, singer info display

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,33 +12,67 @@ import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
 
+    public static Music[] musicList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String[] values = { "song1", "song2", "song3", "song4", "song5",
-                "song6", "song7"};
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        musicList = new Music[]{
+                new Music("overdrive", "post malone", R.raw.overdrive),
+                new Music("circles", "post malone", R.raw.circles),
+                new Music("out of time", "the weeknd", R.raw.out_of_time),
+                new Music("as it was", "harry styles", R.raw.as_it_was),
+                // Add more songs as needed
+        };
+
+        String[] values = new String[musicList.length];
+        for (int i = 0; i < musicList.length; i++) {
+            values[i] = musicList[i].getTitle();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
-
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-//        String item = (String) getListAdapter().getItem(position);
-//        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
-        String item = (String) getListAdapter().getItem(position);
+        String selectedMusicTitle = musicList[position].getTitle();
 
         // Create an intent to start the MusicPlayerActivity
         Intent intent = new Intent(this, MusicPlayerActivity.class);
 
-        // Pass data to the intent
-        intent.putExtra("musicTitle", item);
-        // Add similar lines for album cover and singer information
+        // Pass the selected music title to the intent
+        intent.putExtra("selectedMusicTitle", selectedMusicTitle);
 
         // Start the activity
         startActivity(intent);
     }
+}
 
+class Music {
+    private String title;
+    private String singer;
+    private int resourceId; // ID of the music resource (assuming it's a resource ID)
+
+    // Constructor, getters, setters...\
+    public Music(String title, String singer, int resourceId) {
+        this.title = title;
+        this.singer = singer;
+        this.resourceId = resourceId;
+    }
+
+    // Add getters for title, singer, and resourceId
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
 }
--- app/src/main/java/com/example/myapplication/MusicPlayerActivity.java
+++ app/src/main/java/com/example/myapplication/MusicPlayerActivity.java
@@ -5,13 +5,16 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MusicPlayerActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
-    private String musicTitle;
+    private String selectedMusicTitle;
+    private Music selectedMusic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,22 +35,31 @@ public class MusicPlayerActivity extends AppCompatActivity {
                 if (mediaPlayer != null) {
                     if (mediaPlayer.isPlaying()) {
                         mediaPlayer.pause();
-                        btnPlayPause.setText("Play");
+                        //btnPlayPause.setText("Play");
                     } else {
                         mediaPlayer.start();
-                        btnPlayPause.setText("Pause");
+                        //btnPlayPause.setText("Pause");
                     }
                 }
             }
         });
 
-        // Retrieve data from the intent
+        // Retrieve the selected music title from the intent
         Intent intent = getIntent();
-        musicTitle = intent.getStringExtra("musicTitle");
-        // Add similar lines for album cover and singer information
+        selectedMusicTitle = intent.getStringExtra("selectedMusicTitle");
 
-        startMusicPlayback();
-        // Now, you can use the data in your UI or perform other actions
+        // Find the Music object based on the title
+        selectedMusic = findMusicByTitle(selectedMusicTitle);
+
+        if (selectedMusic != null) {
+            // Initialize UI elements and start music playback
+            initUI();
+            startMusicPlayback();
+        } else {
+            // Handle case where Music object is not found
+            // You may display an error message or take appropriate action
+            finish(); // Finish the activity
+        }
     }
 
     @Override
@@ -59,9 +71,29 @@ public class MusicPlayerActivity extends AppCompatActivity {
         }
     }
 
+    private Music findMusicByTitle(String title) {
+        for (Music music : MainActivity.musicList) {
+            if (music.getTitle().equals(title)) {
+                return music;
+            }
+        }
+        return null;
+    }
+
+    private void initUI() {
+        // Initialize UI elements here (e.g., set text for TextViews)
+        TextView titleTextView = findViewById(R.id.textTitle);
+        TextView singerTextView = findViewById(R.id.textSinger);
+
+        titleTextView.setText(selectedMusic.getTitle());
+        singerTextView.setText(selectedMusic.getSinger());
+    }
+
     private void startMusicPlayback() {
         // Initialize MediaPlayer and load the music file
-        mediaPlayer = MediaPlayer.create(this, R.raw.overdrive); // Replace with your music file
+        mediaPlayer = MediaPlayer.create(this, selectedMusic.getResourceId());
         mediaPlayer.start();
     }
+
+    // Rest of your code...
 }
