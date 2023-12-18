6759944e3a87e8c159ddde45e0291a2f1974fb0c
wonho1111 <109856968+wonho1111@users.noreply.github.com>
2023-12-19 01:17:15 +0900

    play music, play/pause button implement

--- app/src/main/java/com/example/myapplication/MusicPlayerActivity.java
+++ app/src/main/java/com/example/myapplication/MusicPlayerActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,9 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MusicPlayerActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private String musicTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +25,43 @@ public class MusicPlayerActivity extends AppCompatActivity {
             }
         });
 
+        Button btnPlayPause = findViewById(R.id.btnPlayPause);
+        btnPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null) {
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                        btnPlayPause.setText("Play");
+                    } else {
+                        mediaPlayer.start();
+                        btnPlayPause.setText("Pause");
+                    }
+                }
+            }
+        });
+
         // Retrieve data from the intent
         Intent intent = getIntent();
-        String musicTitle = intent.getStringExtra("musicTitle");
+        musicTitle = intent.getStringExtra("musicTitle");
         // Add similar lines for album cover and singer information
 
+        startMusicPlayback();
         // Now, you can use the data in your UI or perform other actions
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    private void startMusicPlayback() {
+        // Initialize MediaPlayer and load the music file
+        mediaPlayer = MediaPlayer.create(this, R.raw.overdrive); // Replace with your music file
+        mediaPlayer.start();
     }
 }
